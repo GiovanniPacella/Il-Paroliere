1c63e109a61da5a005557a64a5344f8eebed69c5
riccardo.angelo <riccardo.angelo@edu.cobianchi.it>
2023-05-06 21:43:11 +0200

    EXIT GIOCA TEST 2

--- Model/Menu.cs
+++ Model/Menu.cs
@@ -17,7 +17,10 @@ namespace Il_Paroliere
 
         private void bottoneGioca_Click(object sender, EventArgs e)
         {
-
+            if (MessageBox.Show("Sei sicuro di voler uscire???", "Messaggio", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                Application.Exit();
+            }
         }
 
         private void Menu_Load(object sender, EventArgs e)

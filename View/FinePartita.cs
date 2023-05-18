91b382e96d9e25f32579c1fbcdf5ca02d8f0a226
GiovanniPacella <pacellagiovanni1@gmail.com>
2023-05-18 12:12:20 +0200

    Aggiornamenti grafici 3.0: Aggiunta pagina finale

---
+++ View/FinePartita.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Il_Paroliere.View
+{
+    public partial class FinePartita : Form
+    {
+        public FinePartita()
+        {
+            InitializeComponent();
+        }
+
+        private void bottoneMenu_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            var menu = new Menu();
+            menu.Closed += (s, args) => this.Close();
+            menu.Show();
+        }
+
+        private void bottoneUscita_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Sei sicuro di voler uscire?", "Uscita", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}

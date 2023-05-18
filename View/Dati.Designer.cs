3e332d1bbac7749696265e173838d4c717eb220e
GiovanniPacella <pacellagiovanni1@gmail.com>
2023-05-18 13:53:29 +0200

    Inserimento canzone del titolo

--- View/Dati.Designer.cs
+++ View/Dati.Designer.cs
@@ -109,7 +109,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Bauhaus 93", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(94)))), ((int)(((byte)(40)))));
-            this.label3.Location = new System.Drawing.Point(938, 482);
+            this.label3.Location = new System.Drawing.Point(908, 482);
             this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(471, 108);

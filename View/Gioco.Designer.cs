12eb8f06b43750ba0c63adc3f802d2671cc2e516
giovanni.pacella <giovanni.pacella@edu.cobianchi.it>
2023-05-20 17:40:46 +0200

    Ripristino impostazioni Gioco.cs originarie

--- View/Gioco.Designer.cs
+++ View/Gioco.Designer.cs
@@ -500,7 +500,8 @@
             // 
             // Gioco
             // 
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Linen;
             this.ClientSize = new System.Drawing.Size(1036, 751);
             this.Controls.Add(this.Reset);

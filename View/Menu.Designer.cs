2e8c2b2de9e89e81fc8311e108259e80de711fba
GiovanniPacella <pacellagiovanni1@gmail.com>
2023-05-20 15:37:26 +0200

    Risoluzione grafica per pc corrente v.2

--- View/Menu.Designer.cs
+++ View/Menu.Designer.cs
@@ -99,7 +99,7 @@
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
-            this.pictureBox1.Location = new System.Drawing.Point(965, 92);
+            this.pictureBox1.Location = new System.Drawing.Point(1066, 90);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(6);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(243, 232);
